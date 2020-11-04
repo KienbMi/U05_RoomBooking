b7c1b94785b5b3ad83931ce6b28470a9b69de5fb
KienbMi <m_kienberger@gmx.net>
2020-11-04 23:55:48 +0100

    validation is finished

--- RoomBooking.Wpf/ViewModels/EditCustomerModel.cs
+++ RoomBooking.Wpf/ViewModels/EditCustomerModel.cs
@@ -1,10 +1,14 @@
-﻿using RoomBooking.Core.Entities;
+﻿using RoomBooking.Core.Contracts;
+using RoomBooking.Core.Entities;
+using RoomBooking.Core.Validations;
+using RoomBooking.Persistence;
 using RoomBooking.Wpf.Common;
 using RoomBooking.Wpf.Common.Contracts;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace RoomBooking.Wpf.ViewModels
@@ -16,6 +20,8 @@ namespace RoomBooking.Wpf.ViewModels
         private string _firstName;
         private string _iban;
 
+        //[MinLength(2, ErrorMessage = "Minimum length of Lastname is 2")]
+        //[Required(ErrorMessage = "Lastname is required")]
         public string LastName 
         {
             get => _lastName;            
@@ -23,14 +29,40 @@ namespace RoomBooking.Wpf.ViewModels
             {
                 _lastName = value;
                 OnPropertyChanged();
+                Validate();
             }
         }
-        public string FirstName { get; set; }
-        public string Iban { get; set; }
+
+        public string FirstName 
+        {
+            get => _firstName; 
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged();
+                Validate();
+            }
+        }
+
+        public string Iban 
+        {
+            get => _iban;           
+            set
+            {
+                _iban = value;
+                OnPropertyChanged();
+                Validate();
+            }
+        }
 
         public EditCustomerModel(IWindowController windowController, Customer customer) : base(windowController)
         {
             _customer = customer;
+            InitViewData();
+        }
+
+        private void InitViewData()
+        {
             LastName = _customer.LastName;
             FirstName = _customer.FirstName;
             Iban = _customer.Iban;
@@ -38,7 +70,96 @@ namespace RoomBooking.Wpf.ViewModels
 
         public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(LastName))
+            {
+                yield return new ValidationResult(
+                    "Lastname is required",
+                    new string[] { nameof(LastName) }
+                    );
+            }
+            else if (LastName.Length < 2)
+            {
+                yield return new ValidationResult(
+                    "Minimum length of Lastname is 2",
+                    new string[] { nameof(LastName) }
+                    );
+            }
+
+            if (!IbanChecker.CheckIban(Iban))
+            {
+                yield return new ValidationResult(
+                    "Iban not valid",
+                    new string[] { nameof(Iban) }
+                    );
+            }
+        }
+
+        private async Task SaveCustomer()
+        {
+            Validate();
+
+            if (IsValid)
+            {
+                using IUnitOfWork uow = new UnitOfWork();
+                _customer.FirstName = FirstName;
+                _customer.LastName = LastName;
+                _customer.Iban = Iban;
+                _customer.Bookings = null;
+                uow.Customers.Update(_customer);
+
+                try
+                {
+                    await uow.SaveAsync();
+                    Controller.CloseWindow(this);
+                }
+                catch (ValidationException validationException)
+                {
+                    if (validationException.Value is IEnumerable<string> properties)
+                    {
+                        foreach (var property in properties)
+                        {
+                            
+                            AddErrorsToProperty(property, new List<string> { validationException.ValidationResult.ErrorMessage });
+                        }
+                    }
+                    else
+                    {
+                        DbError = validationException.ValidationResult.ToString();
+                    }
+                }
+            }
+        }
+
+        //Commands
+        private ICommand _cmdUndo;
+        public ICommand CmdUndo 
+        { 
+            get
+            {
+                if(_cmdUndo == null)
+                {
+                    _cmdUndo = new RelayCommand(
+                        execute: _ => InitViewData(),
+                        canExecute: _ => true);
+                }
+                return _cmdUndo;
+            }
+        }
+
+        private ICommand _cmdSaveChanges;
+        public ICommand CmdSaveChanges 
+        { 
+            get
+            {
+                if(_cmdSaveChanges == null)
+                {
+                    _cmdSaveChanges = new RelayCommand(
+                        execute: async _ => await SaveCustomer(),
+                        canExecute: _ => IsValid
+                        );
+                }
+                return _cmdSaveChanges;
+            }
         }
     }
 }
